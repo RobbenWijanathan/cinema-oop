f4f8feafaad4024baff647363f86d10b0c506da8
Robben Wijanathan <robbenwijanathan@gmail.com>
2025-12-03 17:38:00 +0700

    Update Main.java

--- CinemaOOP/src/Main.java
+++ CinemaOOP/src/Main.java
@@ -7,16 +7,15 @@ import ui_ux.Landing;
 
 public class Main {
     public Main() {
-//    	MySQLConn test;
-//		try {
-//			test = (MySQLConn) MySQLConn.getConn();
-//			Statement st = test.conn.createStatement();
-//		} catch (ClassNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//		Landing landing = new Landing();
+    	MySQLConn test;
+		try {
+			test = (MySQLConn) MySQLConn.getConn();
+			Statement st = test.conn.createStatement();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		Landing.loginRegister();
     }
     public static void main(String[] args){
